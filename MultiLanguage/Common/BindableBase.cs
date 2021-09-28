499cae3e4125b8000d4bd3852d668330255c7595
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2021-09-29 06:40:50 +0800

    Add some common classes

---
+++ MultiLanguage/Common/BindableBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MultiLanguage.Common {
+    /// </summary>
+    /// <remarks>
+    /// https://github.com/Microsoft/Windows-appsample-networkhelper
+    /// </remarks>
+    public abstract class BindableBase : INotifyPropertyChanged {
+        /// <summary>
+        /// 屬性更改通知的多播事件。
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+        /// <summary>
+        /// 檢查屬性是否已與所需值匹配。 設置屬性, 並在必要時通知攔截器。
+        /// </summary>
+        /// <typeparam name="T">屬性的類型。</typeparam>
+        /// <param name="storage">對具有 getter 和 setter 的屬性的引用。</param>
+        /// <param name="value">屬性所需的值。</param>
+        /// <param name="propertyName">用於通知攔截器的屬性的名稱。
+        /// 此值是可選的, 可以在支援 CallerMemberName 的編譯器調用時自動提供。</param>
+        /// <returns>如果值更改為 True, 則如果現有值與所需值匹配, 則為 false。</returns>
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] String? propertyName = null) {
+            if (Object.Equals(storage, value)) {
+                return false;
+            }
+            storage = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+        /// <summary>
+        /// 通知攔截器屬性值已更改。
+        /// </summary>
+        /// <param name="propertyName">用於通知攔截器的屬性的名稱。
+        /// 此值是可選的, 可以在支援 <see cref="CallerMemberNameAttribute"/>
+        /// 的編譯器中調用時自動提供。</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] String? propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MultiLanguage/Common/DelegateCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MultiLanguage.Common {
+    /// <summary>
+    /// Bind actions to the view as commands
+    /// </summary>
+    public class DelegateCommand : ICommand {
+        private readonly Predicate<Object?>? _canExecute;
+        private readonly Action<Object?> _execute;
+
+        public DelegateCommand(Action<Object?> execute, Predicate<Object?>? canExecute = null) {
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        public bool CanExecute(Object? parameter) {
+            return this._canExecute is null || this._canExecute(parameter);
+        }
+
+        public void Execute(Object? parameter) {
+            this._execute(parameter);
+        }
+        //public void RaiseCanExecuteChanged() {
+        //    this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        //}
+
+        public event EventHandler? CanExecuteChanged {
+            add {
+                CommandManager.RequerySuggested += value;
+            }
+            remove { 
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+    }
+}
